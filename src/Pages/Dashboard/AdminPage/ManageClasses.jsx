12f4fd0c5ffbb8ba12ae86ef0c5bdc421c9dfa55
unknown <sobujhasan1000@gmail.com>
2023-06-14 14:21:19 +0600

    class approved status ok

--- src/Pages/Dashboard/AdminPage/ManageClasses.jsx
+++ src/Pages/Dashboard/AdminPage/ManageClasses.jsx
@@ -8,6 +8,33 @@ const ManageClasses = () => {
   });
   console.log(classes);
 
+//   handel approved
+ const handelApprove=id=>{
+    fetch(`http://localhost:5000/classes/approve/${id}`,{
+        method:'PATCH'
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        if(data.modifiedCount){
+            refetch()
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'successful',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        }
+    })
+ }
+
+//  handel denied
+
+const handelDeny=id=>{
+
+}
+
   return (
     <div>
       <h1 className="text-xl text-center my-4">Total classes {classes.length}</h1>
@@ -18,17 +45,17 @@ const ManageClasses = () => {
               <img src={cls.classPhoto} alt="Shoes" />
             </figure>
             <div className="card-body">
-                <h1 className="text-center text-sky-400">Status: {cls.role==='approved'? 'approved': cls.role==='denied'?'denied':'Pending'} </h1>
+                <h1 className="text-center text-sky-400">Status: {cls.status==='approved'? 'approved': cls.status==='denied'?'denied':'Pending'} </h1>
               <h2 className="card-title text-xl">Class Name: {cls.className}</h2>
               <h2 className="">Price: ${cls.price}</h2>
               <h2 className="">Seat Range: {cls.seatRange}</h2> 
               <p> Instructor Name: {cls.instructorName}</p>
               <p> Instructor email: {cls.Inemail}</p>
               <div className="flex justify-around">
-                <button className="bg-sky-200 rounded-md p-2 btn w-24">
+                <button onClick={()=>handelApprove(cls._id)} className="bg-sky-200 rounded-md p-2 btn w-24">
                   Approve
                 </button>
-                <button className="bg-sky-200 rounded-md p-2 btn w-24">
+                <button onClick={()=>handelDeny(cls._id)} className="bg-sky-200 rounded-md p-2 btn w-24">
                   Deny
                 </button>
               </div>
