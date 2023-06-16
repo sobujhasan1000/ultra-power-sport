d672dc68b42a1b3fdfdf545dbdbbd6280708d6ff
unknown <sobujhasan1000@gmail.com>
2023-06-16 15:26:39 +0600

    get selected class ok

--- src/Pages/Dashboard/StudentPage/SelectedClass.jsx
+++ src/Pages/Dashboard/StudentPage/SelectedClass.jsx
@@ -1,6 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAuth from '../../../Customhooks/Auth/useAuth';
 
 const SelectedClass = () => {
+    const {user}=useAuth();
+    console.log('selected',user)
+    const { data: selectedClass = [], refetch } = useQuery(
+        ["selectedClass"],
+        async () => {
+          const res = await fetch(`https://ultra-sport-server.vercel.app/selectedClass?email=${user?.email}`);
+          return res.json();
+        }
+      );
+      console.log(selectedClass)
     return (
         <div>
             <h1>all selected class</h1>
