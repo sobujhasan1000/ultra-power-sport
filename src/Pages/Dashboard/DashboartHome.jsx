0bed6b7b4fe71d8959407497662cdfb18a93e83c
unknown <sobujhasan1000@gmail.com>
2023-06-16 19:17:20 +0600

    selected class delete worke done

--- src/Pages/Dashboard/DashboartHome.jsx
+++ src/Pages/Dashboard/DashboartHome.jsx
@@ -6,7 +6,7 @@ const DashboartHome = () => {
     console.log(user)
     return (
         <div className='bg-slate-300 rounded-full '>
-            <h1 className='text-center text-xl font-bold p-10  h-64 w-64 mt-16'> well come <br />to dashboard Name: {user.displayName} <br /></h1>
+            <h1 className='text-center text-xl font-bold p-10  h-64 w-64 mt-16'> well come <br />to dashboard Name: {user?.displayName} <br /></h1>
         </div>
     );
 };
--- src/Pages/Dashboard/StudentPage/SelectedClass.jsx
+++ src/Pages/Dashboard/StudentPage/SelectedClass.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAuth from '../../../Customhooks/Auth/useAuth';
+import Swal from 'sweetalert2';
 
 const SelectedClass = () => {
     const {user}=useAuth();
@@ -12,13 +13,45 @@ const SelectedClass = () => {
           return res.json();
         }
       );
-      console.log(selectedClass)
+    //   console.log(selectedClass)
+
+    const handelDelet=cls=>{
+        console.log(cls)
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/selectedClass/${cls._id}`,{
+                    method:'DELETE'
+                })
+                .then(res=>res.json())
+                .then(data=>{
+                    if(data.deletedCount>0){
+                        refetch();
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                          )
+                    }
+                })
+            }
+          })
+    }
+
+
     return (
         <div className='bg-green-200 w-full h-full m-2 rounded-md'>
             <h1 className='text-center my-6 text-3xl font-bold'>Your selected class </h1>
             <div className='grid grid-cols-3 gap-4 p-4'>
                 {
-                    selectedClass.map(cls=>
+                    selectedClass?.map(cls=> 
                         <div key={cls._id} className="card bg-neutral text-neutral-content">
                         <div className="card-body items-center text-center">
                           <h2 className="card-title"> course name: {cls.className}</h2>
@@ -26,7 +59,7 @@ const SelectedClass = () => {
                           <p>instractor:   {cls.instructorName}</p>
                           <div className="card-actions justify-end">
                             <button className="btn ">pay now</button>
-                            <button className="btn ">delete</button>
+                            <button onClick={()=>handelDelet(cls)} className="btn ">delete</button>
                           </div>
                         </div>
                       </div>  
